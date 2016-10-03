fd837c588284c38c894f5599edb4fdb9f20aa524
treb <q>
2016-10-04 00:29:52 +0800

    add api methods to new web api project

--- trunk/src/Ugroop.API.SQL_2/Properties/AssemblyInfo.cs
+++ trunk/src/Ugroop.API.SQL_2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ugroop.API.SQL")]
+[assembly: AssemblyTitle("Ugroop.API.SQL_2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ugroop.API.SQL")]
+[assembly: AssemblyProduct("Ugroop.API.SQL_2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

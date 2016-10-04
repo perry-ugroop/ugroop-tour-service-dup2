a485b2a6a1a8edf7cb849fd4bc7c5fc1ee5b385f
Perry Valdez <perry.valdez@ugroop.com.au>
2016-10-04 22:17:16 +0700

    Created XUnit unit test project

---
+++ trunk/src/Ugroop.API.Tests/Ugroop.API.Tests/TestUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ugroop.API.Tests
+{
+    public class TestUnitTest
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}

045b520b594eb53286501859ba0f4810acb7977f
Perry Valdez <perry.valdez@ugroop.com.au>
2017-02-22 16:19:27 +0800

    Adjusted the SQL and batch scripts such that the batch script will be the one in control of the name of the database to be constructed by the SQL script

--- trunk/src/tour_db/dockdb/db_create.bat
+++ trunk/src/tour_db/dockdb/db_create.bat
@@ -5,12 +5,15 @@ SET DB="master"
 SET INPUT="c:\dockdb\dockuploaddb.sql"
 SET LOGIN="sa"
 SET PASSWORD="Password1*"
-SET DBFINAL="uGroopDB2_TEST"
+SET DBFINAL="uGroopDB2"
 
 ECHO ==========EXECUTING SQL SCRIPT==============
-SQLCMD -E -S %SERVER% -d %DB% -i %INPUT%
+ECHO "Running SQL script..." >> C:\db_create.log
+SQLCMD -E -S %SERVER% -d %DB% -i %INPUT% -v DB = %DBFINAL% >> C:\db_create.log  2>&1
 REM change [sa] password here
-SQLCMD -S %SERVER% -d %DBFINAL% -U %LOGIN% -P %PASSWORD%
+ECHO "After running SQL script..." >> C:\db_create.log
+SQLCMD -S %SERVER% -d %DBFINAL% -U %LOGIN% -P %PASSWORD% >> C:\db_create.log  2>&1
+ECHO "All done." >> C:\db_create.log
 ECHO ==========     DONE    ==============
 
 ping localhost -t
